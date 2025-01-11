4f2e17076b329e1b9ee7dd82af19815f3d49b95e
Madhushan13 <96137093+Madhushan13@users.noreply.github.com>
2025-01-11 23:38:39 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Sample tasks for the to-do list
+tasks = [
+    {'id': 1, 'task': 'Learn Flask'},
+    {'id': 2, 'task': 'Build to-do app'},
+    {'id': 3, 'task': 'Deploy to cloud'}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
